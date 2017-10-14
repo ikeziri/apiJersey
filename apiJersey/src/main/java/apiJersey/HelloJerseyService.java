a503267b86f824fa890bc5059da9c0dd290e3dc2
ikezi <ikezi@DESKTOP-3PU0J40>
2017-10-13 21:23:35 -0300

    entrada de dados no teste Jersey

--- apiJersey/src/main/java/apiJersey/HelloJerseyService.java
+++ apiJersey/src/main/java/apiJersey/HelloJerseyService.java
@@ -1,8 +1,10 @@
 package apiJersey;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/helloJersey")
@@ -16,10 +18,10 @@ public class HelloJerseyService {
        @GET
        @Path("/client")
        @Produces(MediaType.APPLICATION_JSON)
-       public Client getCliente() {
+       public Client getCliente(@DefaultValue("111") @QueryParam("numero") int numero) {
              Client client = new Client();
              client.setName("Hello World Camilo");
-             client.setNumero(1);
+             client.setNumero(numero);
              return client;
        }
 }
